3ad41241db77921871b528566046f9f8a0693534
t3chj4ck <haojie.t3ch@outlook.com>
2026-05-25 04:10:03 +0500

    Mon May 25 04:10:02 +05 2026

--- backup.sh
+++ backup.sh
@@ -7,5 +7,5 @@ pacman -Qnq | sort > ~/dotsfile/pkg_pacman
 pacman -Qmq | sort > ~/dotsfile/pkg_aur
 
 git add ~/dotsfile/.
-git commit -m "$my_commit"
+git commit -a -m "$my_commit"
 git push origin main
