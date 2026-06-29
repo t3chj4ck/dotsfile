4b6dcb13955d4b534df2a3aa6e97f4c51d041768
t3chj4ck <haojie.t3ch@outlook.com>
2026-06-29 09:14:43 +0500

    Mon Jun 29 09:14:42 AM +05 2026

--- dots_home/script/power_status.sh
+++ dots_home/script/power_status.sh
@@ -3,4 +3,4 @@ POWER=$(cat /sys/class/power_supply/BAT0/power_now)
 
 POWER=$(echo "scale=2;$POWER / 1000000" | bc)
 
-echo "󱟤 ${POWER}W"
+echo " 󱟤 ${POWER}W "
--- dots_home/script/weather.sh
+++ dots_home/script/weather.sh
@@ -3,7 +3,7 @@
 while true; do
 	result=$(curl -s -f "wttr.in/yekaterinburg?format=%c+%t&m" 2>/dev/null)
 	if [ $? -eq 0 ] && [ -n "$result" ]; then
-		echo "$result"
+		echo " $result "
 		break
 	fi
 	sleep 1
