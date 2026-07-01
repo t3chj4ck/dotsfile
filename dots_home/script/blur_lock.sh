32580572abbb62599f4a7b152e2b541d141f4c2e
t3chj4ck <haojie.t3ch@outlook.com>
2026-07-01 06:56:58 +0500

    Wed Jul  1 06:56:58 AM +05 2026

--- dots_home/script/blur_lock.sh
+++ dots_home/script/blur_lock.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-
 IMAGE=/tmp/swaylock_bg.png
 
-grim - | ffmpeg -i - -vf "boxblur=15:3" -vframes 1 -q:v 2 -y "$IMAGE" 2>/dev/null
+grim - | convert - -blur 0x8 "$IMAGE"
 swaylock -i "$IMAGE"
 
 rm "$IMAGE"
